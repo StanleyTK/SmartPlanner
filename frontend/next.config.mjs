b05d82aa642e10a3c28ba0798474e69bde0c103b
stanleytk <kim4111@purdue.edu>
2025-01-20 16:40:08 -0500

    Github Pages

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: 'export',  // Enable static export for deployment
+    images: {
+      unoptimized: true,  // Required for exporting images in static mode
+    },
+    basePath: '/MyCalendar',  // Set this to your GitHub repo name
+    assetPrefix: '/MyCalendar/',  // Prefix for static assets
+  };
+  
+  export default nextConfig;
+  
