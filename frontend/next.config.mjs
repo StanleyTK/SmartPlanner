67580ce37013768b8ce52c22f481aa5b4b89a2e5
stanleytk <kim4111@purdue.edu>
2025-01-20 17:07:10 -0500

    PLEASE

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? 'export' : undefined,
-    images: {
-      unoptimized: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages',  // Required for exporting images
-    },
-    basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar' : '',
-    assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar/' : '',
+  output: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? 'export' : undefined,
+  images: {
+    unoptimized: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages',  // Required for exporting images
+  },
+  basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar' : '',
+  assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar/' : '',
 };
 
 export default nextConfig;
