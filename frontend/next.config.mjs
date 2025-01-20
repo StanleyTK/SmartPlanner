6deeabf48ba8b2bdbc06d180f0e6b9e4abb3c33e
stanleytk <kim4111@purdue.edu>
2025-01-20 17:01:43 -0500

    Fixed issues with deploying /MyCalendar/MyCalendar

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? 'export' : undefined,
-    images: {
-      unoptimized: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages',  // Required for exporting images
-    },
-    basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar' : '',
-    assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar/' : '',
+  output: 'export',  // Enables static export
+  images: {
+    unoptimized: true,  // Required for exporting images in static mode
+  },
+  basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar' : '',
+  assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar/' : '',
 };
 
 export default nextConfig;
