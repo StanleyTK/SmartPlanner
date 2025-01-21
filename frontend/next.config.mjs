482ac04ad9cfdcca454cf9fca2544a9da1db39c9
stanleytk <kim4111@purdue.edu>
2025-01-21 00:16:55 -0500

    CSS PLEASE

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
     images: {
       unoptimized: true, // Required for GitHub Pages
     },
-    basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar' : '',
-    assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/MyCalendar/' : '',
+    basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner' : '',
+    assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner/' : '',
   };
   
   export default nextConfig;
