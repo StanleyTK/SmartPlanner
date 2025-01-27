347802f54e281226d6fd08b3a249b1d9e4d8e9be
stanleytk <kim4111@purdue.edu>
2025-01-26 22:02:39 -0500

    Env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-      unoptimized: true, // Required for GitHub Pages
-    },
-    basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner' : '',
-    assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner/' : '',
-  };
-  
-  export default nextConfig;
-  
+  output: 'export',
+  images: {
+    unoptimized: true, // Required for GitHub Pages
+  },
+  basePath: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner' : '',
+  assetPrefix: process.env.NEXT_PUBLIC_DEPLOY_TARGET === 'gh-pages' ? '/SmartPlanner/' : '',
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api',
+  },
+};
+
+export default nextConfig;
